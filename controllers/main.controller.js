dcc2791e4fe8deba90ff707093f32b8ea17486fc
dev-maple <info.dev.maple@gmail.com>
2018-09-06 15:38:22 +0200

    converting template done

--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -1,12 +1,48 @@
-
+var sharedData = {
+    menu: 'store'
+}
 class MainController {
+
     constructor() {}
 
     recent(req, res) {
-        res.render('pages/recent-played', {});
+        sharedData.menu = 'recently-played';
+        res.render('pages/recently-played', { sharedData : sharedData });
     }
+
     songs(req, res) {
-        res.render('pages/songs', {});
+        sharedData.menu = 'songs';
+        res.render('pages/songs', { sharedData : sharedData });
+    }
+
+    albums(req, res) {
+        sharedData.menu = 'albums';
+        res.render('pages/albums', { sharedData : sharedData });
+    }
+
+    artists(req, res) {
+        sharedData.menu = 'artists';
+        res.render('pages/artists', { sharedData : sharedData });
+    }
+
+    purchased(req, res) {
+        sharedData.menu = 'purchased';
+        res.render('pages/purchased', { sharedData : sharedData });
+    }
+
+    myLike(req, res) {
+        sharedData.menu = 'my-like';
+        res.render('pages/my-like', { sharedData : sharedData });
+    }
+
+    singleArtist(req, res) {
+        sharedData.menu = 'single-artist';
+        res.render('pages/single-artist', { sharedData : sharedData });
+    }
+
+    singlePlaylist(req, res) {
+        sharedData.menu = 'single-playlist';
+        res.render('pages/single-playlist', { sharedData : sharedData });
     }
 
 }
--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,11 @@ module.exports = function(app) {
     app.use('/store', require('../controllers/store'));
     app.use('/recently-played', main.recent.bind(main));
     app.use('/songs', main.songs.bind(main));
+    app.use('/albums', main.albums.bind(main));
+    app.use('/artists', main.artists.bind(main));
+    app.use('/purchased', main.purchased.bind(main));
+    app.use('/my-like', main.myLike.bind(main));
+    app.use('/single-artist', main.singleArtist.bind(main));
+    app.use('/single-playlist', main.singlePlaylist.bind(main));
 }
     
