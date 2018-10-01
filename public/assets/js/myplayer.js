49ac88cf4e4cef172e73f9ea4a92a6e4855c1299
dev-maple <info.dev.maple@gmail.com>
2018-10-01 22:31:10 +0800

    test

--- public/assets/js/myplayer.js
+++ public/assets/js/myplayer.js
@@ -4,17 +4,19 @@ var adonisPlayer = {},
   adonisPlayerID = 'adonis_jplayer_main',
   adonisPlayerContainer = 'adonis_jp_container',
   adonisPlaylist,
-  currentPlayMusic;
-currentPlayingMusicIndex = 0;
-currentPlayTime = 0;
-var isPlaying = false;
-var playingMode = '';
+  currentPlayMusic,
+  currentPlayingMusicInde0x = 0,
+  currentPlayTime = 0,
+  isPlaying = false,
+  currentPlayingMusicIndex = 0,
+  playingMode = '';
 
 document.addEventListener('musickitloaded', function () {
   applemusic = MusicKit.getInstance();
 });
 
 var getAppleMusicdetail = async function (music_id) {
+
   return await applemusic.api.song(music_id).then((res) => {
     let artwork = res.attributes.artwork;
     let img_url = artwork.url;
@@ -116,8 +118,12 @@ jQuery(document).ready(function ($) {
       isPlaying = true;
       $(".jp-seek-bar").attr('style', 'width: 100%;');
       $("#adonis_jp_container").addClass(" jp-state-playing");
-      if (applemusic.player.isPlaying) { applemusic.player.stop(); }
-      applemusic.setQueue({ song: currentPlayMusic.data.id }).then(function () {
+      if (applemusic.player.isPlaying) {
+        applemusic.player.stop();
+      }
+      applemusic.setQueue({
+        song: currentPlayMusic.data.id
+      }).then(function () {
         applemusic.player.play();
         showingTimeAndProgress();
       });
@@ -153,11 +159,14 @@ jQuery(document).ready(function ($) {
       adonisPlayer.play();
     } else if (playingMode == 'repeat') {
       currentPlayingMusicIndex++;
-      if (currentPlayingMusicIndex == adonisAllPlaylists.length) { currentPlayingMusicIndex = 0; }
+      if (currentPlayingMusicIndex == adonisAllPlaylists.length) {
+        currentPlayingMusicIndex = 0;
+      }
       adonisPlayer.play();
     } else {
-      if (currentPlayingMusicIndex == adonisAllPlaylists.length - 1) { applemusic.player.stop(); }
-      else {
+      if (currentPlayingMusicIndex == adonisAllPlaylists.length - 1) {
+        applemusic.player.stop();
+      } else {
         currentPlayingMusicIndex++;
         adonisPlayer.play();
       }
@@ -173,8 +182,12 @@ jQuery(document).ready(function ($) {
     var lists = "";
     let index = 0;
     adonisAllPlaylists.forEach(music => {
-      let playcss = '', playanimationcss = '';
-      if (currentPlayingMusicIndex == index) { playcss = ' jp-playlist-current'; playanimationcss = ' playing'; }
+      let playcss = '',
+        playanimationcss = '';
+      if (currentPlayingMusicIndex == index) {
+        playcss = ' jp-playlist-current';
+        playanimationcss = ' playing';
+      }
       var list_item = '<li class="item clearfix' + playcss + '"><div class="playlist-item"><div class="img-box music-img-box song-poster" data-playlist-index="' + index + '"><img src="' + music.data.img + '" alt=""><div class="hover-state"><span class="play-btn-dark play-music"><i class="play-icon play-music-list"></i></span></div></div><div class="meta"><span class="now playlist-animate' + playanimationcss + '"><span class="bar n1">A</span><span class="bar n2">B</span><span class="bar n3">c</span></span><div class="hover-state"><div class="d-flex justify-content-end align-items-center"><a href="#" class="mr-2"><span class="adonis-icon"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="18"height="15" viewBox="0 0 37 32"><path d="M27.379 0c-3.478 0.417-6.509 2.067-8.695 4.492l-0.011 0.012c-2.204-2.428-5.231-4.075-8.638-4.498l-0.068-0.007c-6.232 0-9.966 3.641-9.966 9.756 0.377 3.717 2.096 6.973 4.658 9.327l0.011 0.010 13.001 12.534c0.225 0.231 0.539 0.374 0.886 0.374 0.009 0 0.017-0 0.026-0l-0.001 0c0.004 0 0.010 0 0.015 0 0.35 0 0.667-0.143 0.895-0.373l0-0 13.187-12.511c0-0.187 4.668-4.365 4.668-9.36 0-6.115-3.734-9.756-9.966-9.756zM30.763 17.179l-12.090 11.647-12.114-11.67c-2.066-1.882-3.481-4.446-3.89-7.334l-0.008-0.065c0-5.859 3.968-7.002 7.306-7.002s6.605 3.361 7.679 4.668c0.253 0.283 0.619 0.46 1.027 0.46s0.774-0.177 1.026-0.458l0.001-0.001c1.074-1.284 4.668-4.668 7.679-4.668s7.282 1.237 7.282 7.002c0 3.805-3.851 7.352-3.898 7.422z"></path></svg></span></a><a class="track-menu-playlist" href="#"><i class="icon-dot-nav-horizontal"></i></a></div></div> <span class="jp-time">' + convertToMins(music.data.duration) + '</span></div><a href="javascript:;" class="jp-playlist-item">' + music.data.name + '</a><p class="jp-artist"><a href="#link1">' + music.data.artistName + '</a></p></div></li>';
       lists += list_item;
       index++;
@@ -204,7 +217,7 @@ jQuery(document).ready(function ($) {
     if (isPlaying && currentPlayTime < maxtime - 1) {
       setTimeout(showingTimeAndProgress, 1000);
     }
-    
+
   }
 
   var timeDrag = false; /* Drag status */
@@ -236,6 +249,7 @@ jQuery(document).ready(function ($) {
       updatebar(percentage);
     }
   });
+
   function updateMusic(percentage) {
     //update music time line
     if (currentPlayMusic.src == 'apple') {
@@ -349,6 +363,9 @@ jQuery(document).ready(function ($) {
       $("#loginForm").submit();
     });
   });
+  $("#loginwithSportify").click(function () {
+    
+  });
 
   // ----------------------------------------------------------------------------------------------------------------------
 
@@ -369,8 +386,7 @@ jQuery(document).ready(function ($) {
           adonisPlayer.updatePlaylists();
         })
       }
-    }
-    else if (target.html() == 'Add to Playlist') {
+    } else if (target.html() == 'Add to Playlist') {
       let url = '/home/music/playlist/getlists';
       //get all playlists
       $.ajax({
@@ -434,7 +450,14 @@ jQuery(document).ready(function ($) {
       method: 'POST',
       crossDomain: true,
       data: {
-        music_id, music_src, music_type, music_user_token, mode, playlist_id, playlistName, playlistDescription
+        music_id,
+        music_src,
+        music_type,
+        music_user_token,
+        mode,
+        playlist_id,
+        playlistName,
+        playlistDescription
       },
       success: function (res) {
         $("#addtoPlaylistModal").modal('toggle');
